93aa5384f79ed7afecb538f0e5ecbb67936cd773
ruddro420 <aliruddro4@gmail.com>
2023-12-22 16:55:29 +0600

    product ready

--- resources/views/layouts/admin/adminLayout.blade.php
+++ resources/views/layouts/admin/adminLayout.blade.php
@@ -58,7 +58,7 @@ $route = Route::current()->getName();
             Nav header start
         ***********************************-->
         <div class="nav-header">
-            <a href="index.html" class="brand-logo">
+            <a href="{{route('dashboard')}}" class="brand-logo">
                 D-CMS
                 <!--  <img class="logo-abbr" src="images/logo-white.png" alt="">
                 <img class="logo-compact" src="images/logo-text-white.png" alt="">
@@ -82,115 +82,16 @@ $route = Route::current()->getName();
             <div class="header-content">
                 <nav class="navbar navbar-expand">
                     <div class="collapse navbar-collapse justify-content-between">
-                        <div class="header-left">
-                            <div class="search_bar dropdown">
-                                <span class="search_icon p-3 c-pointer" data-toggle="dropdown">
-                                    <i class="mdi mdi-magnify"></i>
-                                </span>
-                                <div class="dropdown-menu p-0 m-0">
-                                    <form>
-                                        <input class="form-control" type="search" placeholder="Search"
-                                            aria-label="Search">
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-
+                        <div class="header-left"></div>
                         <ul class="navbar-nav header-right">
-                            <li class="nav-item dropdown notification_dropdown">
-                                <a class="nav-link bell ai-icon" href="#" role="button" data-toggle="dropdown">
-                                    <svg id="icon-user" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
-                                        <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-                                        <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-                                    </svg>
-                                    <div class="pulse-css"></div>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <ul class="list-unstyled">
-                                        <li class="media dropdown-item">
-                                            <span class="success"><i class="ti-user"></i></span>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    <p><strong>Martin</strong> has added a <strong>customer</strong>
-                                                        Successfully
-                                                    </p>
-                                                </a>
-                                            </div>
-                                            <span class="notify-time">3:20 am</span>
-                                        </li>
-                                        <li class="media dropdown-item">
-                                            <span class="primary"><i class="ti-shopping-cart"></i></span>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    <p><strong>Jennifer</strong> purchased Light Dashboard 2.0.</p>
-                                                </a>
-                                            </div>
-                                            <span class="notify-time">3:20 am</span>
-                                        </li>
-                                        <li class="media dropdown-item">
-                                            <span class="danger"><i class="ti-bookmark"></i></span>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    <p><strong>Robin</strong> marked a <strong>ticket</strong> as
-                                                        unsolved.
-                                                    </p>
-                                                </a>
-                                            </div>
-                                            <span class="notify-time">3:20 am</span>
-                                        </li>
-                                        <li class="media dropdown-item">
-                                            <span class="primary"><i class="ti-heart"></i></span>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    <p><strong>David</strong> purchased Light Dashboard 1.0.</p>
-                                                </a>
-                                            </div>
-                                            <span class="notify-time">3:20 am</span>
-                                        </li>
-                                        <li class="media dropdown-item">
-                                            <span class="success"><i class="ti-image"></i></span>
-                                            <div class="media-body">
-                                                <a href="#">
-                                                    <p><strong> James.</strong> has added a<strong>customer</strong>
-                                                        Successfully
-                                                    </p>
-                                                </a>
-                                            </div>
-                                            <span class="notify-time">3:20 am</span>
-                                        </li>
-                                    </ul>
-                                    <a class="all-notification" href="#">See all notifications <i
-                                            class="ti-arrow-right"></i></a>
-                                </div>
-                            </li>
                             <li class="nav-item dropdown header-profile">
                                 <a class="nav-link" href="#" role="button" data-toggle="dropdown">
-                                    <img src="{{asset('admin/images/profile/education/pic1.jpg')}}" width="20" alt="">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/4086/4086679.png" width="20"
+                                        alt="">
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right">
-                                    <a href="app-profile.html" class="dropdown-item ai-icon">
-                                        <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                                            viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
-                                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                            <circle cx="12" cy="7" r="4"></circle>
-                                        </svg>
-                                        <span class="ml-2">Profile </span>
-                                    </a>
-                                    <a href="email-inbox.html" class="dropdown-item ai-icon">
-                                        <svg id="icon-inbox" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                                            viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
-                                            <path
-                                                d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
-                                            </path>
-                                            <polyline points="22,6 12,13 2,6"></polyline>
-                                        </svg>
-                                        <span class="ml-2">Inbox </span>
-                                    </a>
-                                    <a href="page-login.html" class="dropdown-item ai-icon">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
                                         <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
                                             viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                             stroke-linecap="round" stroke-linejoin="round"
@@ -199,8 +100,11 @@ $route = Route::current()->getName();
                                             <polyline points="16 17 21 12 16 7"></polyline>
                                             <line x1="21" y1="12" x2="9" y2="12"></line>
                                         </svg>
-                                        <span class="ml-2">Logout </span>
-                                    </a>
+                                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                            this.closest('form').submit();">
+                                            {{ __('Log Out') }}
+                                        </x-dropdown-link>
+                                    </form>
                                 </div>
                             </li>
                         </ul>
@@ -284,19 +188,6 @@ $route = Route::current()->getName();
                             <li><a href="{{route('result.add')}}">Add Result / Notice</a></li>
                         </ul>
                     </li>
-                    <li class="nav-label">Website Maintaince</li>
-                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">
-                            <i class="la la-bullhorn"></i>
-                            <span class="nav-text">Page</span>
-                        </a>
-                        <ul aria-expanded="false">
-                            <li><a href="{{route('result.view')}}">Home</a></li>
-                            <li><a href="{{route('result.add')}}">About</a></li>
-                            <li><a href="{{route('result.add')}}">Courses</a></li>
-                            <li><a href="{{route('result.add')}}">Notice</a></li>
-                            <li><a href="{{route('result.add')}}">Admission </a></li>
-                        </ul>
-                    </li>
                 </ul>
             </div>
         </div>
@@ -312,7 +203,8 @@ $route = Route::current()->getName();
         ***********************************-->
         <div class="footer">
             <div class="copyright">
-                <p>Copyright © Designed &amp; Developed by <a href="../index.htm" target="_blank">Tech I Trick</a> 2023
+                <p>Copyright © Designed &amp; Developed by <a href="../index.htm" target="_blank">Ali Fiad Ruddro</a>
+                    2023
                 </p>
             </div>
         </div>
@@ -339,10 +231,12 @@ $route = Route::current()->getName();
     ***********************************-->
 
     {{-- Sweet Alert CDN --}}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+        integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
-    
+
     <!-- Required vendors -->
     <script src="{{ asset('admin/vendor/global/global.min.js')}} "></script>
     <script src="{{ asset('admin/vendor/bootstrap-select/dist/js/bootstrap-select.min.js')}} "></script>
@@ -369,16 +263,16 @@ $route = Route::current()->getName();
     <script src="{{ asset('admin/vendor/svganimation/vivus.min.js')}} "></script>
     <script src="{{ asset('admin/vendor/svganimation/svg.animation.js')}} "></script>
     <script src="{{ asset('admin/js/styleSwitcher.js')}} "></script>
-    	<!-- pickdate -->
-        <script src="{{asset('admin/vendor/pickadate/picker.js')}}"></script>
-        <script src="{{asset('admin/vendor/pickadate/picker.time.js')}}"></script>
-        <script src="{{asset('admin/vendor/pickadate/picker.date.js')}}"></script>
-        
-        <!-- Pickdate -->
-        <script src="{{asset('admin/js/plugins-init/pickadate-init.js')}}"></script>
-        {{-- Sweet Alert for before download --}}
-        <script>
-            $(function() {
+    <!-- pickdate -->
+    <script src="{{asset('admin/vendor/pickadate/picker.js')}}"></script>
+    <script src="{{asset('admin/vendor/pickadate/picker.time.js')}}"></script>
+    <script src="{{asset('admin/vendor/pickadate/picker.date.js')}}"></script>
+
+    <!-- Pickdate -->
+    <script src="{{asset('admin/js/plugins-init/pickadate-init.js')}}"></script>
+    {{-- Sweet Alert for before download --}}
+    <script>
+        $(function() {
                 $(document).on('click', '#delete', function(e) {
                   e.preventDefault();
                     var link = $(this).attr("href");
@@ -402,9 +296,9 @@ $route = Route::current()->getName();
                     })
                 });
             });
-          </script>
-     @notifyJs
-    
+    </script>
+    @notifyJs
+
 </body>
 
 </html>
